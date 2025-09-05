d69225a54478a15709c5a74880962cd5d6f1d78b
LiquidDragoon <30419058+LiquidDragoon@users.noreply.github.com>
2025-09-05 15:38:12 -0700

    Delete poc-test-api/poc-test-api directory

--- poc-test-api/poc-test-api/src/main/java/com/coppel/dto/logging/AdditionalInfo.java
+++
@@ -1,9 +0,0 @@
-package com.coppel.dto.logging;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-
-public @Data class AdditionalInfo {
-    @JsonProperty("clientID")
-    private int clientId;
-}
--- poc-test-api/poc-test-api/src/main/java/com/coppel/dto/logging/Measurement.java
+++
@@ -1,8 +0,0 @@
-package com.coppel.dto.logging;
-
-import lombok.Data;
-
-public @Data class Measurement {
-    private String method;
-    private long elapsedTime;
-}
